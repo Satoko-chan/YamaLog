3a7318ede4764cb3deb4efc2cd9a7c614ecbdce4
Satoko-chan <49394103+Satoko-chan@users.noreply.github.com>
2020-08-19 12:37:10 +0900

    modify humberger menu design

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,40 +26,25 @@ $(document).on('turbolinks:load', function () {
         let navItem = $('.topNav a');
         if ($(this).scrollTop() >= 80 && 150 >= $(this).scrollTop()) {
             topMessage.css({ 'color': "#ff5757" })
-            // navItem.css({ 'color': "#ff5757" })
         } else if ($(this).scrollTop() > 150 && 1200 > $(this).scrollTop()) {
             topMessage.css({ 'color': "#ff1616" })
-            // navItem.css({ 'color': "#ff1616" })
-
         } else if ($(this).scrollTop() >= 1200) {
             topMessage.css({ 'color': "#000" })
-            // navItem.css({ 'color': "#000" })
         } else {
             topMessage.css({ 'color': "#000" })
-            // navItem.css({ 'color': "#000" })
         }
     });
 
-    $(window).scroll(function () {
-        let homeAboutBg = $('.gradationBg');
-        if ($(this).scrollTop() >= 0 && 500 >= $(this).scrollTop()) {
-            homeAboutBg.css({ 'background-color': "#a1d6e2" })
-        } else if ($(this).scrollTop() > 500) {
-            homeAboutBg.css({ 'background-color': "#e4d5d3" })
-        } else {
-            homeAboutBg.css({ 'background-color': "#fce1d3" })
-        }
-    });
-
-    // window.addEventListener('scroll', () => {
-    //     //idがkeyvisualの要素を取得
-    //     let elem = document.getElementById('home_top_container');
-    //     //現在のスクロール位置を取得して、10で除算
-    //     let scrollY = window.scrollY / 10;
-    //     //上で取得した値と画像幅を加算して、elemの背景サイズに設定
-    //     elem.style.backgroundSize = 100 + scrollY + '%';
+    // $(window).scroll(function () {
+    //     let homeAboutBg = $('.gradationBg');
+    //     if ($(this).scrollTop() >= 0 && 500 >= $(this).scrollTop()) {
+    //         homeAboutBg.css({ 'background-color': "#a1d6e2" })
+    //     } else if ($(this).scrollTop() > 500) {
+    //         homeAboutBg.css({ 'background-color': "#e4d5d3" })
+    //     } else {
+    //         homeAboutBg.css({ 'background-color': "#fce1d3" })
+    //     }
     // });
-
 
     var nav = $('#navArea');
     var btn = $('.toggle_btn');
