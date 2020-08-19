4dc13be72a0b838763de9311a7c9e350f211861d
Satoko-chan <49394103+Satoko-chan@users.noreply.github.com>
2020-08-19 12:14:46 +0900

    added humberger nav menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -61,6 +61,23 @@ $(document).on('turbolinks:load', function () {
     // });
 
 
+    var nav = $('#navArea');
+    var btn = $('.toggle_btn');
+    var mask = $('#mask');
+    var open = 'open'; // class
+    // menu open close
+    btn.on('click', function () {
+        if (!nav.hasClass(open)) {
+            nav.addClass(open);
+        } else {
+            nav.removeClass(open);
+        }
+    });
+    // mask close
+    mask.on('click', function () {
+        nav.removeClass(open);
+    });
+
 });
 
 
